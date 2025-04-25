29a34b85bd42426d1519bb025c6154c225ef9860
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-04-25 13:22:59 +0100

    init project

---
+++ src/main/java/com/znaji/accountsmicroservices/AccountsMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.znaji.accountsmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsMicroServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsMicroServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/znaji/accountsmicroservices/AccountsMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.znaji.accountsmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsMicroServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
